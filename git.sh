aaca0f735add37e810149d01d051768b31509b62
mountainriver <olxd@hotmail.com>
2017-04-16 17:07:04 +0800

    green

--- git.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo "Adding all change..."
-git add -A
-echo "Commiting..."
-git commit -m "$1"
-echo "Pushing..."
-git push
---
+++ github_m.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+echo -e "\033[32mGithub working...\033[0m"
+echo -e "\033[32mAdding all change...\033[0m"
+git add -A
+echo -e "\033[32mCommiting...\033[0m"
+git commit -m "$1"
+echo -e "\033[32mPushing...\033[0m"
+git push
