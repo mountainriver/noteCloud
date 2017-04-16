579c676a4b1e7c84e19592f16cc319eae06d94a7
mountainriver <olxd@hotmail.com>
2017-04-16 17:57:36 +0800

    ok

--- github_m.sh
+++ github_m.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 echo -e "\033[32mGithub working...\033[0m"
-echo -e "\033[32mAdding all change...\033[0m"
+echo -e "\033[32m1.Adding all change...\033[0m"
 git add -A
-echo -e "\033[32mCommiting...\033[0m"
+echo -e "\033[32m2.Commiting...\033[0m"
 git commit -m "$1"
-echo -e "\033[32mPushing...\033[0m"
+echo -e "\033[32m3.Pushing...\033[0m"
 git push
 echo -e "\033[32mDone.\033[0m"
