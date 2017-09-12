65c0d56c68373f18c8eb61a8337bb4a9bd1a8804
mountainriver <olxd@hotmail.com>
2017-09-12 20:34:00 +0800

    4

--- github_m.sh
+++ github_m.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 echo -e "\033[32mGithub working...\033[0m"
+echo -e "\033[32m0.pulling...\033[0m"
+git pull
 echo -e "\033[32m1.Adding all change...\033[0m"
 git add -A
 echo -e "\033[32m2.Commiting...\033[0m"
