8903bd1691dbb8049c3716abebd3af74c4cf0a03
Jordan <olxd@hotmail.com>
2018-01-11 13:25:07 +0800

    2018011125

--- github_m.sh
+++ github_m.sh
@@ -1,13 +1,20 @@
 #/bin/bash
 
-t=`date +%Y%m%d%M`
 echo -e "\033[32mGithub working...\033[0m"
 echo -e "\033[32m0.pulling...\033[0m"
 git pull
 echo -e "\033[32m1.Adding all change...\033[0m"
 git add -A
 echo -e "\033[32m2.Commiting...\033[0m"
-git commit -m "$t"
+
+if [-n "$1"]
+then
+	git commit -m "$1"
+else
+	t=`date +%Y%m%d%M`
+	git commit -m "$t"
+fi
+
 echo -e "\033[32m3.Pushing...\033[0m"
 git push
 echo -e "\033[32mDone.\033[0m"
